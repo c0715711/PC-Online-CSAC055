a79e23644b9036536acf6db35fad0dc897e814b6
c0715711 <50410115+c0715711@users.noreply.github.com>
2019-06-27 15:40:28 -0400

    week8

---
+++ add_category.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Captcha
+{
+    public partial class add_category : System.Web.UI.Page
+    {
+
+        public add_category()
+        {
+
+        }
+        public string cnstring = "Data Source=HARISHHARRY267\\SQLEXPRESS;Initial Catalog=praveen;Integrated Security=True";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string myquery = "select * from [dbo].[category]";
+            SqlConnection con = new SqlConnection(cnstring);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = myquery;
+            cmd.Connection = con;
+
+            SqlDataAdapter da = new SqlDataAdapter();//communication b/w Dataset and SQL DB
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();//it is kind of a box,use for single database tabales
+
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm2.aspx");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cnstring);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                string a = "insert into [dbo].[category](category_Id,category_name,numberof_products) values('" + TextBox2.Text.ToString() + "','" + TextBox1.Text.ToString() + "','"+ TextBox3.Text.ToString() + "')";
+                SqlCommand cmd = new SqlCommand(a, con);
+                cmd.ExecuteNonQuery();
+
+                // this("added successfully");
+ 
+                string myquery = "select * from [dbo].[category]";
+                SqlConnection con1 = new SqlConnection(cnstring);
+                SqlCommand insertcmd = new SqlCommand(myquery, con1);
+                cmd.CommandText = myquery;
+                cmd.Connection = con1;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                //SqlDataAdapter da = new SqlDataAdapter(cmd);//communication b/w Dataset and SQL DB
+                                                            //da.SelectCommand = cmd;
+
+                DataSet ds = new DataSet();//it is kind of a box,use for single database tabales
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                con.Close();
+            }
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+        }
+
+       
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cnstring);
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                string update = "delete from dbo.category where category_Id=@id";
+                SqlCommand cmd = new SqlCommand(update, con);
+                cmd.ExecuteNonQuery();
+                Response.Write("alert('Data deleted')");
+            }
+        }
+    }
+}

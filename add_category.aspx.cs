ce796bc5555717135423da4293f80a3cc663c174
c0715711 <50410115+c0715711@users.noreply.github.com>
2019-07-25 14:26:07 -0400

    week12

--- add_category.aspx.cs
+++ add_category.aspx.cs
@@ -19,7 +19,12 @@ namespace Captcha
         public string cnstring = "Data Source=HARISHHARRY267\\SQLEXPRESS;Initial Catalog=praveen;Integrated Security=True";
 
         protected void Page_Load(object sender, EventArgs e)
+
         {
+           // if (Session["Username"] == null)
+            //{
+              //  Response.Redirect("WebForm2.aspx");
+            //}
             string myquery = "select * from [dbo].[category]";
             SqlConnection con = new SqlConnection(cnstring);
             SqlCommand cmd = new SqlCommand();
@@ -28,16 +33,20 @@ namespace Captcha
 
             SqlDataAdapter da = new SqlDataAdapter();//communication b/w Dataset and SQL DB
             da.SelectCommand = cmd;
+
             DataSet ds = new DataSet();//it is kind of a box,use for single database tabales
 
             da.Fill(ds);
-            GridView1.DataSource = ds;
             GridView1.DataBind();
+
+
 
         }
 
         protected void LinkButton1_Click(object sender, EventArgs e)
         {
+            //Session["Username"] = null;
+            //Session["Password"] = null;
             Response.Redirect("WebForm2.aspx");
         }
 
@@ -47,12 +56,12 @@ namespace Captcha
             con.Open();
             if (con.State == System.Data.ConnectionState.Open)
             {
-                string a = "insert into [dbo].[category](category_Id,category_name,numberof_products) values('" + TextBox2.Text.ToString() + "','" + TextBox1.Text.ToString() + "','"+ TextBox3.Text.ToString() + "')";
+                string a = "insert into [dbo].[category](category_Id,category_name,numberof_products) values('" + TextBox2.Text.ToString() + "','" + TextBox1.Text.ToString() + "','" + TextBox3.Text.ToString() + "')";
                 SqlCommand cmd = new SqlCommand(a, con);
                 cmd.ExecuteNonQuery();
 
                 // this("added successfully");
- 
+
                 string myquery = "select * from [dbo].[category]";
                 SqlConnection con1 = new SqlConnection(cnstring);
                 SqlCommand insertcmd = new SqlCommand(myquery, con1);
@@ -61,44 +70,21 @@ namespace Captcha
 
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 //SqlDataAdapter da = new SqlDataAdapter(cmd);//communication b/w Dataset and SQL DB
-                                                            //da.SelectCommand = cmd;
+                //da.SelectCommand = cmd;
 
                 DataSet ds = new DataSet();//it is kind of a box,use for single database tabales
                 da.Fill(ds);
-                GridView1.DataSource = ds;
+
                 GridView1.DataBind();
-                con.Close();
             }
         }
 
-        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
-        {
 
-        }
 
-        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
-        {
 
-        }
 
-        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
-        {
 
-        }
 
-       
-
-        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
-        {
-            SqlConnection con = new SqlConnection(cnstring);
-            con.Open();
-            if (con.State == System.Data.ConnectionState.Open)
-            {
-                string update = "delete from dbo.category where category_Id=@id";
-                SqlCommand cmd = new SqlCommand(update, con);
-                cmd.ExecuteNonQuery();
-                Response.Write("alert('Data deleted')");
-            }
-        }
     }
+
 }
